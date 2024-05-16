aec285192961c2ba66368ceda5e7823d5bb6976a
Bozhidar <bobicloudvision@gmail.com>
2024-05-16 22:51:03 +0300

    update

--- installers/ubuntu-22.04/install-partial/download_web.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/PanelOmega/WebCompiledVersions/raw/main/panel-omega-latest.zip
-unzip -qq -o panel-omega-latest.zip -d /usr/local/omega/web
-rm -rf panel-omega-latest.zip
-
-chmod 711 /home
-chmod -R 750 /usr/local/omega
--- installers/ubuntu-22.04/install.sh
+++ installers/ubuntu-22.04/install.sh
@@ -92,7 +92,7 @@ cd /usr/local/omega/web
 
 # Create MySQL user
 MYSQL_OMEGA_ROOT_USERNAME="omega"
-MYSQL_OMEGA_ROOT_PASSWORD="$(openssl rand -hex 16)"
+MYSQL_OMEGA_ROOT_PASSWORD="$(tr -dc a-za-z0-9 </dev/urandom | head -c 32; echo)"
 
 mysql -uroot -proot <<MYSQL_SCRIPT
   CREATE USER '$MYSQL_OMEGA_ROOT_USERNAME'@'%' IDENTIFIED BY '$MYSQL_OMEGA_ROOT_PASSWORD';
@@ -102,7 +102,7 @@ MYSQL_SCRIPT
 
 
 # Create database
-PANEL_OMEGA_DB_PASSWORD="$(openssl rand -hex 16)"
+PANEL_OMEGA_DB_PASSWORD="$(tr -dc a-za-z0-9 </dev/urandom | head -c 32; echo)"
 PANEL_OMEGA_DB_NAME="omega_$(tr -dc a-za-z0-9 </dev/urandom | head -c 13; echo)"
 PANEL_OMEGA_DB_USER="omega_$(tr -dc a-za-z0-9 </dev/urandom | head -c 13; echo)"
 
@@ -116,20 +116,16 @@ MYSQL_SCRIPT
 mysql_secure_installation --use-default
 
 # Change mysql root password
-echo "Changing mysql root password..."
-MYSQL_ROOT_PASSWORD="$(openssl rand -hex 16)"
+MYSQL_ROOT_PASSWORD="$(tr -dc a-za-z0-9 </dev/urandom | head -c 32; echo)"
 mysql -uroot -proot <<MYSQL_SCRIPT
-  ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+  ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by '$MYSQL_ROOT_PASSWORD';
   FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
 # Save mysql root password
-echo "Save mysql root password..."
 echo "$MYSQL_ROOT_PASSWORD" > /root/.mysql_root_password
 
 # Configure the application
-echo "Configure the application..."
-
 omega-php artisan omega:set-ini-settings APP_ENV "local"
 omega-php artisan omega:set-ini-settings APP_URL "127.0.0.1:8443"
 omega-php artisan omega:set-ini-settings APP_NAME "PANEL_OMEGA"
@@ -149,7 +145,7 @@ omega-php artisan omega:set-ini-settings APP_ENV "production"
 chmod -R o+w /usr/local/omega/web/storage/
 chmod -R o+w /usr/local/omega/web/bootstrap/cache/
 
-CURRENT_IP=$(hostname -I | awk '{print $1}')
+CURRENT_IP=$(curl -s ipinfo.io/ip)
 
 echo "PanelOmega downloaded successfully."
 echo "Please visit http://$CURRENT_IP:8443 to continue installation of the panel."

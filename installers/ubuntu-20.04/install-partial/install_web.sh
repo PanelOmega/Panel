a039c9a35b7abafddac3a9949f0cd313a3169e1e
Bozhidar <bobicloudvision@gmail.com>
2024-05-16 22:49:22 +0300

    Update install_web.sh

--- installers/ubuntu-20.04/install-partial/install_web.sh
+++ installers/ubuntu-20.04/install-partial/install_web.sh
@@ -11,7 +11,7 @@ cd /usr/local/omega/web
 
 # Create MySQL user
 MYSQL_OMEGA_ROOT_USERNAME="omega"
-MYSQL_OMEGA_ROOT_PASSWORD="$(openssl rand -hex 16)"
+MYSQL_OMEGA_ROOT_PASSWORD="$(tr -dc a-za-z0-9 </dev/urandom | head -c 32; echo)"
 
 mysql -uroot -proot <<MYSQL_SCRIPT
   CREATE USER '$MYSQL_OMEGA_ROOT_USERNAME'@'%' IDENTIFIED BY '$MYSQL_OMEGA_ROOT_PASSWORD';
@@ -21,7 +21,7 @@ MYSQL_SCRIPT
 
 
 # Create database
-PANEL_OMEGA_DB_PASSWORD="$(openssl rand -hex 16)"
+PANEL_OMEGA_DB_PASSWORD="$(tr -dc a-za-z0-9 </dev/urandom | head -c 32; echo)"
 PANEL_OMEGA_DB_NAME="omega_$(tr -dc a-za-z0-9 </dev/urandom | head -c 13; echo)"
 PANEL_OMEGA_DB_USER="omega_$(tr -dc a-za-z0-9 </dev/urandom | head -c 13; echo)"
 
@@ -35,9 +35,9 @@ MYSQL_SCRIPT
 mysql_secure_installation --use-default
 
 # Change mysql root password
-MYSQL_ROOT_PASSWORD="$(openssl rand -hex 16)"
+MYSQL_ROOT_PASSWORD="$(tr -dc a-za-z0-9 </dev/urandom | head -c 32; echo)"
 mysql -uroot -proot <<MYSQL_SCRIPT
-  ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+  ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by '$MYSQL_ROOT_PASSWORD';
   FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
@@ -64,7 +64,7 @@ omega-php artisan omega:set-ini-settings APP_ENV "production"
 chmod -R o+w /usr/local/omega/web/storage/
 chmod -R o+w /usr/local/omega/web/bootstrap/cache/
 
-CURRENT_IP=$(hostname -I | awk '{print $1}')
+CURRENT_IP=$(curl -s ipinfo.io/ip)
 
 echo "PanelOmega downloaded successfully."
 echo "Please visit http://$CURRENT_IP:8443 to continue installation of the panel."

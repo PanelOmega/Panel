7d231eb6050e95b47da891865b31b47ca6046f1a
Bozhidar <bobicloudvision@gmail.com>
2024-05-25 00:29:04 +0300

    update

--- web/app/Models/Customer.php
+++ web/app/Models/Customer.php
@@ -3,9 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Customer extends Model
+class Customer extends Authenticatable
 {
+    use HasApiTokens;
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'username',
+        'password',
+        'email',
+        'phone',
+        'address',
+        'city',
+        'state',
+        'zip',
+        'country',
+        'company',
+    ];
+
+    public function hostingSubscriptions()
+    {
+        return $this->hasMany(HostingSubscription::class);
+    }
+
+    public function canBeImpersonated()
+    {
+        return true;
+    }
+
 }

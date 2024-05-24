d82e5827c0fc86323919a05df1de02208b464bca
Bozhidar <bobicloudvision@gmail.com>
2024-05-24 21:04:05 +0300

    update

--- web/tests/Unit/DomainTest.php
+++ web/tests/Unit/DomainTest.php
@@ -3,41 +3,18 @@
 namespace Tests\Unit;
 
 use App\Models\Domain;
-use App\Server\Installers\Virtualization\DockerInstaller;
-use App\Virtualization\Docker\DockerApi;
 use Tests\TestCase;
+use Tests\Unit\Traits\HasDocker;
 
 class DomainTest extends TestCase
 {
+    use HasDocker;
     /**
      * A basic unit test example.
      */
     public function testDomainCreation(): void
     {
-        $checkDockerIsInstalled = DockerApi::isDockerInstalled();
-        if ($checkDockerIsInstalled['status'] === 'error') {
-
-            // Install docker
-            $install = new DockerInstaller();
-            $installStatus = $install->run();
-
-            $this->assertArrayHasKey('status', $installStatus);
-            $this->assertSame('Install job is running in the background.', $installStatus['status']);
-
-            $isDockerInstalled = false;
-            for ($i = 0; $i < 200; $i++) {
-                if (is_file($installStatus['logPath'])) {
-                    $dockerInstallLog = file_get_contents($installStatus['logPath']);
-                    if (str_contains($dockerInstallLog, 'Docker is installed successfully!')) {
-                        $isDockerInstalled = true;
-                        break;
-                    }
-                }
-                sleep(1);
-            }
-            $this->assertTrue($isDockerInstalled);
-
-        }
+        $this->installDocker();
 
         $domainDetails = [
             'domain' => 'example.com',
@@ -49,7 +26,6 @@ class DomainTest extends TestCase
         $domain->save();
 
         $this->assertDatabaseHas('domains', $domainDetails);
-
 
     }
 }
---
+++ web/tests/Unit/Traits/HasDocker.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit\Traits;
+
+use App\Server\Installers\Virtualization\DockerInstaller;
+use App\Virtualization\Docker\DockerApi;
+
+trait HasDocker
+{
+    public function installDocker()
+    {
+        $checkDockerIsInstalled = DockerApi::isDockerInstalled();
+        if ($checkDockerIsInstalled['status'] === 'error') {
+
+            // Install docker
+            $install = new DockerInstaller();
+            $installStatus = $install->run();
+
+            $this->assertArrayHasKey('status', $installStatus);
+            $this->assertSame('Install job is running in the background.', $installStatus['status']);
+
+            $isDockerInstalled = false;
+            for ($i = 0; $i < 200; $i++) {
+                if (is_file($installStatus['logPath'])) {
+                    $dockerInstallLog = file_get_contents($installStatus['logPath']);
+                    if (str_contains($dockerInstallLog, 'Docker is installed successfully!')) {
+                        $isDockerInstalled = true;
+                        break;
+                    }
+                }
+                sleep(1);
+            }
+            $this->assertTrue($isDockerInstalled);
+
+        }
+    }
+}

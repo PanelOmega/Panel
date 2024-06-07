7ee8b6a04a5bac7c64e5a0215cdd2bf7fd61f4a8
Bozhidar <bobicloudvision@gmail.com>
2024-06-07 03:23:14 +0300

    update

--- web/app/Server/Installers/VirtualHosts/ApacheInstaller.php
+++ web/app/Server/Installers/VirtualHosts/ApacheInstaller.php
@@ -1,7 +1,84 @@
 <?php
 
-namespace App\Server\Installers\Web;
+namespace App\Server\Installers\VirtualHosts;
 
 class ApacheInstaller
 {
+
+    public string $logPath = '/var/log/omega/apache-installer.log';
+
+
+    public static function isApacheInstalled(): array
+    {
+        $dockerVersion = shell_exec('apache2 -v');
+        if (str_contains($dockerVersion, 'Apache2 version')) {
+            return [
+                'status' => 'success',
+                'message' => 'Apache2 is installed.',
+                'version' => $dockerVersion,
+            ];
+        }
+
+        return [
+            'status' => 'error',
+            'message' => 'Apache2 is not installed.',
+        ];
+    }
+
+    public function run()
+    {
+        $commands = [];
+        $commands[] = 'echo "Starting Apache Installation..."';
+        $commands[] = 'export DEBIAN_FRONTEND=noninteractive';
+        $commands[] = 'apt-get install -yq sudo';
+        $commands[] = 'add-apt-repository -y ppa:ondrej/apache2';
+
+        $dependenciesList = [
+            'apache2',
+            'apache2-suexec-custom',
+            'libapache2-mod-ruid2'
+        ];
+
+
+        $dependencies = implode(' ', $dependenciesList);
+        $commands[] = 'apt-get install -yq ' . $dependencies;
+
+        $commands[] = 'a2enmod cgi';
+        $commands[] = 'a2enmod mime';
+        $commands[] = 'a2enmod rewrite';
+        $commands[] = 'a2enmod env';
+        $commands[] = 'a2enmod ssl';
+        $commands[] = 'a2enmod actions';
+        $commands[] = 'a2enmod headers';
+        $commands[] = 'a2enmod suexec';
+        $commands[] = 'a2enmod ruid2';
+        $commands[] = 'a2enmod proxy';
+        $commands[] = 'a2enmod proxy_http';
+
+        // For Fast CGI
+        $commands[] = 'a2enmod fcgid';
+        $commands[] = 'a2enmod alias';
+        $commands[] = 'a2enmod proxy_fcgi';
+//        $commands[] = 'a2enmod setenvif';
+
+        // $commands[] = 'ufw allow in "Apache Full"';
+        $commands[] = 'systemctl restart apache2';
+
+        $shellFileContent = '';
+        foreach ($commands as $command) {
+            $shellFileContent .= $command . PHP_EOL;
+        }
+        $shellFileContent .= 'echo "All packages installed successfully!"' . PHP_EOL;
+        $shellFileContent .= 'echo "DONE!"' . PHP_EOL;
+        $shellFileContent .= 'rm -f /tmp/apache-installer.sh';
+
+        file_put_contents('/tmp/apache-installer.sh', $shellFileContent);
+        shell_exec('bash /tmp/apache-installer.sh >> ' . $this->logPath . ' &');
+
+        return [
+            'status' => 'Install job is running in the background.',
+            'message' => 'Apache2 is being installed in the background. Please check the log file for more details.',
+            'logPath' => $this->logPath,
+        ];
+    }
 }
--- web/tests/Unit/Traits/HasDocker.php
+++ web/tests/Unit/Traits/HasDocker.php
@@ -2,12 +2,39 @@
 
 namespace Tests\Unit\Traits;
 
+use App\Server\Installers\VirtualHosts\ApacheInstaller;
 use App\Server\Installers\Virtualization\DockerInstaller;
 
 trait HasDocker
 {
     public function installDocker()
     {
+
+        $checkApache2IsInstalled = ApacheInstaller::isApacheInstalled();
+        if ($checkApache2IsInstalled['status'] === 'error') {
+
+            // Install apache2
+            $install = new ApacheInstaller();
+            $installStatus = $install->run();
+
+            $this->assertArrayHasKey('status', $installStatus);
+            $this->assertSame('Install job is running in the background.', $installStatus['status']);
+
+            $isApache2Installed = false;
+            for ($i = 0; $i < 200; $i++) {
+                if (is_file($installStatus['logPath'])) {
+                    $apache2InstallLog = file_get_contents($installStatus['logPath']);
+                    if (str_contains($apache2InstallLog, 'Apache2 is installed successfully!')) {
+                        $isApache2Installed = true;
+                        break;
+                    }
+                }
+                sleep(1);
+            }
+
+            $this->assertTrue($isApache2Installed);
+        }
+
         $checkDockerIsInstalled = DockerInstaller::isDockerInstalled();
         if ($checkDockerIsInstalled['status'] === 'error') {
 

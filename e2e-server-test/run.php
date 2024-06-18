4fc975a12f4ade2be291262105387523c6eca088
Bozhidar <bobicloudvision@gmail.com>
2024-06-18 23:22:38 +0300

    Update run.php

--- e2e-server-test/run.php
+++ e2e-server-test/run.php
@@ -90,10 +90,10 @@ $application->register('test')
             }
         }
 
+//
         $serverType = $hetznerClient->serverTypes()->get($serverTypeId);
         $location = $hetznerClient->locations()->getByName('fsn1');
-        //$image = $hetznerClient->images()->getByName('ubuntu-22.04');
-        $image = $hetznerClient->images()->getByName('alma-9');
+        $image = $hetznerClient->images()->getByName('ubuntu-22.04');
         $apiResponse = $hetznerClient->servers()->createInLocation($serverName, $serverType, $image, $location, [$hetznerSSHName]);
         $server = $apiResponse->getResponsePart('server');
         $action = $apiResponse->getResponsePart('action');

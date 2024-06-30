a5380d745bd1aa39a8884dc85ee1fb6fc6fa8fc3
Bozhidar <bobicloudvision@gmail.com>
2024-07-01 01:02:55 +0300

    Update web.php

--- web/routes/web.php
+++ web/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return redirect('/');
+})->name('login');

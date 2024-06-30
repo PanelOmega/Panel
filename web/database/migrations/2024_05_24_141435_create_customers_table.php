dd64c17ca6ccc0943bb3827bd5e06bd2190abe66
Bozhidar <bobicloudvision@gmail.com>
2024-07-01 00:50:38 +0300

    update

--- web/database/migrations/2024_05_24_141435_create_customers_table.php
+++ web/database/migrations/2024_05_24_141435_create_customers_table.php
@@ -26,7 +26,23 @@ return new class extends Migration
             $table->string('country')->nullable();
             $table->string('company')->nullable();
 
+            $table->rememberToken();
             $table->timestamps();
+        });
+
+        Schema::create('customer_password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('customer_sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('customer_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
         });
     }
 
@@ -36,5 +52,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('customers');
+        Schema::dropIfExists('customer_password_reset_tokens');
+        Schema::dropIfExists('customer_sessions');
     }
 };

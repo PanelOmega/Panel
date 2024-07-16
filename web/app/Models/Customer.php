d45d1b823cf9dce5c1f2c8708b9a3c578835bc92
Bozhidar <bobicloudvision@gmail.com>
2024-07-16 10:53:51 +0300

    update

--- web/app/Models/Customer.php
+++ web/app/Models/Customer.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use Laravel\Sanctum\HasApiTokens;
 
 class Customer extends Authenticatable
@@ -58,5 +60,21 @@ class Customer extends Authenticatable
     public function canBeImpersonated()
     {
         return true;
+    }
+
+    public static function getHostingSubscriptionSession()
+    {
+        $hostingSubscriptionId = Session::get('hosting_subscription_id');
+        $customerId = Auth::guard('customer')->user()->id;
+        $findHostingSubscription = HostingSubscription::where('customer_id', $customerId)
+            ->where('id', $hostingSubscriptionId)
+            ->first();
+        if (!$findHostingSubscription) {
+            $findHostingSubscription = HostingSubscription::where('customer_id', $customerId)
+                ->first();
+            Session::put('hosting_subscription_id', $findHostingSubscription->id);
+        }
+
+        return $findHostingSubscription;
     }
 }

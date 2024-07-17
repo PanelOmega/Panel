fda4f7415d1fa3500ef7a741edb0397f5d23cdb3
Bozhidar <bobicloudvision@gmail.com>
2024-07-17 18:34:14 +0300

    Update web.php

--- web/routes/web.php
+++ web/routes/web.php
@@ -9,8 +9,6 @@ Route::get('/', function () {
 Route::get('/customer/phpMyAdmin/login', [\App\Http\Controllers\Customer\PHPMyAdminController::class, 'login'])
 ->name('customer.phpmyadmin.login');
 
-Route::get('/customer/phpMyAdmin/validate-token', [\App\Http\Controllers\Customer\PHPMyAdminController::class, 'validateToken'])
-    ->name('customer.phpmyadmin.validate-token');
 
 Route::get('/customer/file-manager', function () {
     return view('customer.pages.file-manager');
@@ -21,9 +19,9 @@ Route::get('/login', function () {
     return redirect('/');
 })->name('login');
 
-
-Route::get('/file-manager/initialize', [FileManagerController::class, 'initialize']);
-
-Route::get('/file-manager/tree', [FileManagerController::class, 'tree']);
-
-Route::get('/file-manager/content', [FileManagerController::class, 'content']);
+//
+//Route::get('/file-manager/initialize', [FileManagerController::class, 'initialize']);
+//
+//Route::get('/file-manager/tree', [FileManagerController::class, 'tree']);
+//
+//Route::get('/file-manager/content', [FileManagerController::class, 'content']);

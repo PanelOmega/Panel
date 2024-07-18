343067c272b74ce6f1b8ef13dd0b65b878dbe654
Bozhidar <bobicloudvision@gmail.com>
2024-07-18 13:22:20 +0300

    Update file-manager.blade.php

--- web/resources/views/customer/pages/file-manager.blade.php
+++ web/resources/views/customer/pages/file-manager.blade.php
@@ -7,6 +7,9 @@
     <title>
         PanelOne - File Manager
     </title>
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">

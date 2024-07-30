3d6a836611e9317f4708e79c673f6836b2895f17
Bozhidar <bobicloudvision@gmail.com>
2024-07-30 22:59:28 +0300

    Update Fail2Ban.php

--- web/app/Filament/Clusters/Fail2Ban/Fail2Ban.php
+++ web/app/Filament/Clusters/Fail2Ban/Fail2Ban.php
@@ -7,4 +7,12 @@ use Filament\Clusters\Cluster;
 class Fail2Ban extends Cluster
 {
     protected static ?string $navigationIcon = 'heroicon-o-shield-check';
+
+    protected static ?string $navigationLabel = 'Fail2Ban';
+
+    protected static ?string $slug = 'fail2ban';
+
+    protected static ?string $title = 'Fail2Ban';
+
+    protected static ?int $navigationSort = 10;
 }

6a6dacfaae67b8a9ed0386cbd14f158bb41ce8d0
Bozhidar <bobicloudvision@gmail.com>
2024-08-09 03:12:00 +0300

    Update install.sh

--- web/server/my-apache/install.sh
+++ web/server/my-apache/install.sh
@@ -1,14 +1,17 @@
-CURRENT_DIR=$(pwd)
-rm -rf $CURRENT_DIR/httpd
-cd $CURRENT_DIR/httpd
+rm -rf httpd-2.4.62-2.el9.x86_64.rpm
+rm -rf httpd-core-2.4.62-2.el9.x86_64.rpm
+rm -rf httpd-tools-2.4.62-2.el9.x86_64.rpm
+rm -rf httpd-filesystem-2.4.62-2.el9.noarch.rpm
+
 wget https://github.com/PanelOmega/Dist/raw/main/compilators/almalinux/my-apache/rpms/x86_64/httpd-2.4.62-2.el9.x86_64.rpm
 wget https://github.com/PanelOmega/Dist/raw/main/compilators/almalinux/my-apache/rpms/x86_64/httpd-core-2.4.62-2.el9.x86_64.rpm
 wget https://github.com/PanelOmega/Dist/raw/main/compilators/almalinux/my-apache/rpms/x86_64/httpd-tools-2.4.62-2.el9.x86_64.rpm
 wget https://github.com/PanelOmega/Dist/raw/main/compilators/almalinux/my-apache/rpms/noarch/httpd-filesystem-2.4.62-2.el9.noarch.rpm
 
-cd $CURRENT_DIR
-
-dnf install -y $CURRENT_DIR/httpd/httpd-2.4.62-2.el9.x86_64.rpm $CURRENT_DIR/httpd/httpd-core-2.4.62-2.el9.x86_64.rpm $CURRENT_DIR/httpd/httpd-tools-2.4.62-2.el9.x86_64.rpm $CURRENT_DIR/httpd/httpd-filesystem-2.4.62-2.el9.noarch.rpm
+dnf install -y httpd-filesystem-2.4.62-2.el9.noarch.rpm
+dnf install -y httpd-core-2.4.62-2.el9.x86_64.rpm
+dnf install -y httpd-tools-2.4.62-2.el9.x86_64.rpm
+dnf install -y httpd-2.4.62-2.el9.x86_64.rpm
 
 systemctl enable httpd
 systemctl start httpd

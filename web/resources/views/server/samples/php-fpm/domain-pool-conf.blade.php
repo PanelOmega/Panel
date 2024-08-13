8416c3001e310c2fff0a778e3ea35357f23e9a89
Bozhidar <bobicloudvision@gmail.com>
2024-08-13 21:24:59 +0300

    Create domain-pool-conf.blade.php

---
+++ web/resources/views/server/samples/php-fpm/domain-pool-conf.blade.php
@@ -0,0 +1,20 @@
+[{{$poolName}}]
+
+; Unix user/group of processes
+user = {{$username}}
+group = {{$username}}
+
+; The address on which to accept FastCGI requests
+listen = 127.0.0.1:{{$port}}
+
+; Set permissions for Unix socket
+listen.owner = {{$username}}
+listen.group = {{$username}}
+listen.mode = 0660
+
+; Process manager settings
+pm = dynamic
+pm.max_children = 10
+pm.start_servers = 3
+pm.min_spare_servers = 2
+pm.max_spare_servers = 5

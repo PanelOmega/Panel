9ad70b9ffad7750adca8af31d895f3c46cca6b18
Bozhidar <bobicloudvision@gmail.com>
2024-08-15 00:22:34 +0300

    add linux webuser resource

---
+++ web/app/Filament/Resources/LinuxWebUserResource/Pages/ListLinuxWebUsers.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\LinuxWebUserResource\Pages;
+
+use App\Filament\Resources\LinuxWebUserResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListLinuxWebUsers extends ListRecords
+{
+    protected static string $resource = LinuxWebUserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}

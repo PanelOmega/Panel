d15567249608cd29c27c31499c33c705c5d2c5da
Bozhidar <bobicloudvision@gmail.com>
2024-08-14 23:25:26 +0300

    update

--- web/app/Models/HostingSubscription.php
+++ web/app/Models/HostingSubscription.php
@@ -35,102 +35,102 @@ class HostingSubscription extends Model
     {
         parent::boot();
 
-        static::creating(function ($model) {
-
-            $findDomain = Domain::where('domain', $model->domain)->first();
-            if ($findDomain) {
-                throw new \Exception('Domain already exists');
-            }
-
-            $create = $model->_createLinuxWebUser($model);
-            if (isset($create['error'])) {
-                throw new \Exception($create['message']);
-            }
-
-            if (isset($create['system_username']) && isset($create['system_password'])) {
-                $model->system_username = $create['system_username'];
-                $model->system_password = $create['system_password'];
-                $model->system_user_id = $create['system_user_id'];
-            } else {
-                return false;
-            }
-
-        });
-
-        static::created(function ($model) {
-
-            $makeMainDomain = new Domain();
-            $makeMainDomain->hosting_subscription_id = $model->id;
-            $makeMainDomain->domain = $model->domain;
-            $makeMainDomain->is_main = 1;
-            $makeMainDomain->status = Domain::STATUS_ACTIVE;
-            $makeMainDomain->save();
-
-        });
-
-        static::deleting(function ($model) {
-
-            if (empty($model->system_username)) {
-                throw new \Exception('System username is empty');
-            }
-
-            $getLinuxUserStatus = LinuxUser::getUser($model->system_username);
-            if (empty($getLinuxUserStatus)) {
-                throw new \Exception('System username not found');
-            }
-
-            $getFptUser = HostingSubscriptionFtpAccount::where('ftp_username', $model->system_username)->get();
-
-            if (!$getFptUser->isEmpty()) {
-                $getFptUser->delete();
-            }
-
-            $findRelatedDomains = Domain::where('hosting_subscription_id', $model->id)->get();
-            if ($findRelatedDomains->count() > 0) {
-                foreach ($findRelatedDomains as $domain) {
-                    $domain->delete();
-                }
-            }
-
-            // Delete databases
-            $databases = Database::where('hosting_subscription_id', $model->id)->get();
-            if ($databases->count() > 0) {
-                foreach ($databases as $database) {
-                    // Delete database users
-                    $databaseUsers = DatabaseUser::where('database_id', $database->id)->get();
-                    if ($databaseUsers->count() > 0) {
-                        foreach ($databaseUsers as $databaseUser) {
-                            $databaseUser->delete();
-                        }
-                    }
-                    $database->delete();
-                }
-            }
-            // Delete main database user
-            $universalDatabaseExecutor = new UniversalDatabaseExecutor(
-                OmegaConfig::get('MYSQL_HOST', '127.0.0.1'),
-                OmegaConfig::get('MYSQL_PORT', 3306),
-                OmegaConfig::get('MYSQL_ROOT_USERNAME'),
-                OmegaConfig::get('MYSQL_ROOT_PASSWORD'),
-            );
-
-            // Check main database user exists
-            $mainDatabaseUser = $universalDatabaseExecutor->getUserByUsername($model->system_username);
-            if (!$mainDatabaseUser) {
-                $deleteMainDatabaseUser = $universalDatabaseExecutor->deleteUserByUsername($model->system_username);
-                if (!isset($deleteMainDatabaseUser['success'])) {
-                    //throw new \Exception($deleteMainDatabaseUser['message']);
-                }
-            }
-
-            // Delete linux user
-            LinuxUser::deleteUser($model->system_username);
-
-            // This must be in background
-            $apacheBuild = new ApacheBuild();
-            $apacheBuild->handle();
-
-        });
+//        static::creating(function ($model) {
+//
+//            $findDomain = Domain::where('domain', $model->domain)->first();
+//            if ($findDomain) {
+//                throw new \Exception('Domain already exists');
+//            }
+//
+//            $create = $model->_createLinuxWebUser($model);
+//            if (isset($create['error'])) {
+//                throw new \Exception($create['message']);
+//            }
+//
+//            if (isset($create['system_username']) && isset($create['system_password'])) {
+//                $model->system_username = $create['system_username'];
+//                $model->system_password = $create['system_password'];
+//                $model->system_user_id = $create['system_user_id'];
+//            } else {
+//                return false;
+//            }
+//
+//        });
+//
+//        static::created(function ($model) {
+//
+//            $makeMainDomain = new Domain();
+//            $makeMainDomain->hosting_subscription_id = $model->id;
+//            $makeMainDomain->domain = $model->domain;
+//            $makeMainDomain->is_main = 1;
+//            $makeMainDomain->status = Domain::STATUS_ACTIVE;
+//            $makeMainDomain->save();
+//
+//        });
+//
+//        static::deleting(function ($model) {
+//
+//            if (empty($model->system_username)) {
+//                throw new \Exception('System username is empty');
+//            }
+//
+//            $getLinuxUserStatus = LinuxUser::getUser($model->system_username);
+//            if (empty($getLinuxUserStatus)) {
+//                throw new \Exception('System username not found');
+//            }
+//
+//            $getFptUser = HostingSubscriptionFtpAccount::where('ftp_username', $model->system_username)->get();
+//
+//            if (!$getFptUser->isEmpty()) {
+//                $getFptUser->delete();
+//            }
+//
+//            $findRelatedDomains = Domain::where('hosting_subscription_id', $model->id)->get();
+//            if ($findRelatedDomains->count() > 0) {
+//                foreach ($findRelatedDomains as $domain) {
+//                    $domain->delete();
+//                }
+//            }
+//
+//            // Delete databases
+//            $databases = Database::where('hosting_subscription_id', $model->id)->get();
+//            if ($databases->count() > 0) {
+//                foreach ($databases as $database) {
+//                    // Delete database users
+//                    $databaseUsers = DatabaseUser::where('database_id', $database->id)->get();
+//                    if ($databaseUsers->count() > 0) {
+//                        foreach ($databaseUsers as $databaseUser) {
+//                            $databaseUser->delete();
+//                        }
+//                    }
+//                    $database->delete();
+//                }
+//            }
+//            // Delete main database user
+//            $universalDatabaseExecutor = new UniversalDatabaseExecutor(
+//                OmegaConfig::get('MYSQL_HOST', '127.0.0.1'),
+//                OmegaConfig::get('MYSQL_PORT', 3306),
+//                OmegaConfig::get('MYSQL_ROOT_USERNAME'),
+//                OmegaConfig::get('MYSQL_ROOT_PASSWORD'),
+//            );
+//
+//            // Check main database user exists
+//            $mainDatabaseUser = $universalDatabaseExecutor->getUserByUsername($model->system_username);
+//            if (!$mainDatabaseUser) {
+//                $deleteMainDatabaseUser = $universalDatabaseExecutor->deleteUserByUsername($model->system_username);
+//                if (!isset($deleteMainDatabaseUser['success'])) {
+//                    //throw new \Exception($deleteMainDatabaseUser['message']);
+//                }
+//            }
+//
+//            // Delete linux user
+//            LinuxUser::deleteUser($model->system_username);
+//
+//            // This must be in background
+//            $apacheBuild = new ApacheBuild();
+//            $apacheBuild->handle();
+//
+//        });
 
     }
 
---
+++ web/app/Services/HostingSubscription/HostingSubscriptionService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services\HostingSubscription;
+
+class CreateHostingSubscriptionService
+{
+    /**
+     * Create a new hosting subscription
+     * @param string $domain
+     * @param int $customerId
+     * @param int $hostingPlanId
+     * @param string $systemUsername
+     * @param string $systemPassword
+     * @return void
+     */
+    public function create(string $domain, int $customerId, int $hostingPlanId, string $systemUsername, string $systemPassword)
+    {
+
+        
+
+    }
+}

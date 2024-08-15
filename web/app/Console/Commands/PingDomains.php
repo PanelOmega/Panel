2d53be484d4e87ff4a3f1381e776ee273d07dfa3
Bozhidar <bobicloudvision@gmail.com>
2024-08-15 11:19:08 +0300

    Create PingDomains.php

---
+++ web/app/Console/Commands/PingDomains.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Domain;
+use Illuminate\Console\Command;
+
+class PingDomains extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'phyre:ping-domains';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Ping all domains in the database and check if they are up or down.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        $getDomains = Domain::get();
+
+        foreach ($getDomains as $domainData) {
+
+            $domain = $domainData->domain;
+
+            $cmd = "curl -s -o /dev/null -w '%{http_code}' http://$domain";
+            $response = shell_exec($cmd);
+            if ($response == 200) {
+                $this->info("Domain $domain is up and running");
+            } else {
+                $this->warn("Domain $domain is down");
+            }
+
+        }
+
+        return 0;
+    }
+}

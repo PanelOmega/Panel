759fd3c4a4c4b108b9a3971300e407d4dc5a23a3
Bozhidar <bobicloudvision@gmail.com>
2024-08-15 14:01:03 +0300

    Update ui-test.blade.php

--- web/resources/views/filament-customer/pages/ui-test.blade.php
+++ web/resources/views/filament-customer/pages/ui-test.blade.php
@@ -10,4 +10,36 @@
         {{$this->form}}
     </div>
 
+    <div>
+        You can use tree select like this:
+        <br />
+        <br />
+<pre>
+use App\Filament\Forms\Components\TreeSelect;
+
+TreeSelect::make('folder')
+    ->live()
+    ->options([
+        [
+            'name' => 'Folder 1',
+            'value' => 'folder-1',
+            'children' => [
+                [
+                'name' => 'Subfolder 1',
+                'value' => 'subfolder-1',
+                ],
+                [
+                'name' => 'Subfolder 2',
+                'value' => 'subfolder-2',
+                ],
+            ],
+        ],
+    [
+        'name' => 'Folder 2',
+        'value' => 'folder-2',
+    ],
+])
+</pre>
+    </div>
+
 </x-filament-panels::page>

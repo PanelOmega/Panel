b89856c8232506c56c64f22767a7626a09074afc
Alex <aleksandarshishkov@abv.bg>
2024-08-30 14:41:55 +0300

    update

--- web/app/Models/Fail2BanBannedIp.php
+++ web/app/Models/Fail2BanBannedIp.php
@@ -5,56 +5,66 @@ namespace App\Models;
 use App\Services\Fail2Ban\Fail2BanBannedIp\Fail2BanBannedIpService;
 use Carbon\CarbonInterval;
 use Illuminate\Database\Eloquent\Model;
+use Sushi\Sushi;
 
 class Fail2BanBannedIp extends Model
 {
-//    use Sushi;
+    use Sushi;
 
     protected $fillable = [
-        'hosting_subscription_id',
         'ip',
         'status',
         'service',
-        'ban_count',
         'banned_date',
         'banned_time',
+    ];
+
+    protected $schema = [
+        'id' => 'integer',
+        'ip' => 'string',
+        'status' => 'string',
+        'service' => 'string',
+        'ban_date' => 'string',
+        'ban_time' => 'string',
     ];
 
     public static function boot()
     {
         parent::boot();
-        static::Fail2BanBannedIpBoot();
-    }
 
-    public static function Fail2BanBannedIpBoot()
-    {
-        $hostingSubscription = Customer::getHostingSubscriptionSession();
-
-        static::creating(function ($model) use ($hostingSubscription) {
+        static::creating(function ($model) {
             $result = Fail2BanBannedIpService::banIP($model->ip, $model->service);
-            if (!$result) {
-                throw new \Exception('Failed to ban IP');
-            }
-
-            $bannedIp = Fail2BanBannedIpService::getBannedIp();
-
-            $model->hosting_subscription_id = $hostingSubscription->id;
-            $model->status = $bannedIp['status'];
-            $model->ban_count = $bannedIp['ban_count'];
-            $model->ban_date = $bannedIp['ban_date'];
-            $model->ban_time = self::secondsToHumanReadable($bannedIp['ban_time']);
-
+//            if (!$result) {
+//                throw new \Exception('Failed to ban IP');
+//            }
         });
 
         static::deleting(function ($model) {
             $result = Fail2BanBannedIpService::unBanIP($model->ip, $model->service);
-            if (!$result) {
-                throw new \Exception('Failed to unban IP');
-            }
+//            if (!$result) {
+//                throw new \Exception('Failed to unban IP');
+//            }
         });
     }
 
-    public static function secondsToHumanReadable($seconds)
+    public function getRows()
+    {
+        $bannedIps = Fail2BanBannedIpService::getIps();
+
+        return array_map(function ($bannedIps, $index) {
+            return [
+                'id' => $index + 1,
+                'ip' => $bannedIps['ip'],
+                'status' => $bannedIps['status'],
+                'service' => $bannedIps['service'],
+                'ban_date' => $bannedIps['ban_date'],
+                'ban_time' => $this->secondsToHumanReadable($bannedIps['ban_time'])
+            ];
+        }, $bannedIps, array_keys($bannedIps));
+
+    }
+
+    public function secondsToHumanReadable($seconds)
     {
         return CarbonInterval::seconds($seconds)->cascade()->forHumans();
     }

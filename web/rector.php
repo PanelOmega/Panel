065be194ec57cc90b436f92d40179bad43a343ba
Bozhidar <bobicloudvision@gmail.com>
2024-08-30 18:20:23 +0300

    update

--- web/rector.php
+++ web/rector.php
@@ -10,19 +10,22 @@ include __DIR__ . '/rector/ConvertLocalVariablesNameToCamelCaseRector.php';
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/app',
-//        __DIR__ . '/app/Models/Fail2BanWhitelistedIp.php',
-        __DIR__ . '/bootstrap',
-        __DIR__ . '/config',
-        __DIR__ . '/public',
-        __DIR__ . '/resources',
-        __DIR__ . '/routes',
-//        __DIR__ . '/test-docker',
-        __DIR__ . '/tests',
+//        __DIR__ . '/app',
+        __DIR__ . '/app/Models/Fail2BanWhitelistedIp.php',
+//        __DIR__ . '/bootstrap',
+//        __DIR__ . '/config',
+//        __DIR__ . '/public',
+//        __DIR__ . '/resources',
+//        __DIR__ . '/routes',
+//        __DIR__ . '/tests',
     ])
     // uncomment to reach your current PHP version
 
 
+    ->withSets([
+        \RectorLaravel\Set\LaravelSetList::LARAVEL_CODE_QUALITY,
+        \RectorLaravel\Set\LaravelSetList::LARAVEL_110,
+    ])
         ->withRules([
         ConvertProtectedMethodsNameToCamelCaseRector::class,
         ConvertPrivateMethodsNameToCamelCaseRector::class,

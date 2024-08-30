832ab5f23c4d96d5c1d0d00467a6af5ca48548b0
Bozhidar <bobicloudvision@gmail.com>
2024-08-31 01:15:56 +0300

    update

--- web/tests/Unit/Models/HostingSubscription/HostingSubscriptionTest.php
+++ web/tests/Unit/Models/HostingSubscription/HostingSubscriptionTest.php
@@ -7,8 +7,10 @@ use App\Models\Customer;
 use App\Models\Domain;
 use App\Models\HostingPlan;
 use App\Models\HostingSubscription;
+use App\Server\Helpers\PHP;
 use App\Server\SupportedApplicationTypes;
 use App\Services\HostingSubscription\HostingSubscriptionService;
+use Illuminate\Support\Str;
 use Illuminate\Testing\Fluent\Concerns\Has;
 use Tests\TestCase;
 use Tests\Unit\Traits\HasDocker;
@@ -26,7 +28,6 @@ class HostingSubscriptionTest extends TestCase
         $this->installDocker();
         $this->installPHP();
 
-
         $customerUsername = 'test' . rand(1000, 9999);
 
         $createCustomer = new Customer();
@@ -37,46 +38,49 @@ class HostingSubscriptionTest extends TestCase
         $createCustomer->save();
         $this->assertDatabaseHas('customers', ['username' => $customerUsername]);
 
+        // Test Hosting Subscription Creation with all installed PHP versions
+        $getInstalledPHPVersions = PHP::getInstalledPHPVersions();
+        foreach ($getInstalledPHPVersions as $phpVersion) {
 
-        $createHostingPlan = new HostingPlan();
-        $createHostingPlan->name = 'test' . rand(1000, 9999);
-        $createHostingPlan->default_server_application_type = 'apache_php';
-        $createHostingPlan->default_server_application_settings = [
-            'php_version' => '8.1',
-        ];
-        $createHostingPlan->save();
-        $this->assertDatabaseHas('hosting_plans', ['name' => $createHostingPlan->name]);
+            $createHostingPlan = new HostingPlan();
+            $createHostingPlan->name = 'test' . rand(1000, 9999);
+            $createHostingPlan->default_server_application_type = 'apache_php';
+            $createHostingPlan->default_server_application_settings = [
+                'php_version' => $phpVersion['full'],
+                'enable_php_fpm' => true,
+            ];
+            $createHostingPlan->save();
+            $this->assertDatabaseHas('hosting_plans', ['name' => $createHostingPlan->name]);
 
-        $hostingSubscriptionService = new HostingSubscriptionService();
-        $createResponse = $hostingSubscriptionService->create(
-            'test' . rand(1000, 9999) . '.demo.panelomega-unit.com',
-            $createCustomer->id,
-            $createHostingPlan->id,
-            null,
-            null
-        );
-        $this->assertTrue($createResponse['success']);
-        $hostingSubscription = $createResponse['hostingSubscription'];
+            $hostingSubscriptionService = new HostingSubscriptionService();
+            $createResponse = $hostingSubscriptionService->create(
+                'test' . rand(1000, 9999) . '.demo.panelomega-unit.com',
+                $createCustomer->id,
+                $createHostingPlan->id,
+                null,
+                null
+            );
+            $this->assertTrue($createResponse['success']);
+            $hostingSubscription = $createResponse['hostingSubscription'];
 
-        $this->assertDatabaseHas('hosting_subscriptions', ['domain' => $hostingSubscription->domain]);
+            $this->assertDatabaseHas('hosting_subscriptions', ['domain' => $hostingSubscription->domain]);
 
-        static::$lastCreatedHostingSubscriptionId = $hostingSubscription->id;
-//
-//        $apacheBuild = new ApacheBuild();
-//        $apacheBuild->handle();
-//
-        $findDomain = Domain::where('hosting_subscription_id', $hostingSubscription->id)->first();
-        $this->assertDatabaseHas('domains', ['domain' => $findDomain->domain]);
+            static::$lastCreatedHostingSubscriptionId = $hostingSubscription->id;
 
-        // Test domain php version
-//        shell_exec('sudo echo "0.0.0.0 '.$findDomain->domain_public.'" | sudo tee -a /etc/hosts');
-/*        file_put_contents($findDomain->domain_public . '/index.php', '<?php echo "site-is-ok, "; echo phpversion(); ?>');*/
-//        $domainHomePageContent = file_get_contents('http://' . $hostingSubscription->domain);
+            $findDomain = Domain::where('hosting_subscription_id', $hostingSubscription->id)->first();
+            $this->assertDatabaseHas('domains', ['domain' => $findDomain->domain]);
 
-//        dd($domainHomePageContent);
-//        $this->assertTrue(Str::contains($domainHomePageContent, 'site-is-ok, 5.6'));
+            // Test domain php version
+            shell_exec('sudo echo "127.0.0.1 '.$findDomain->domain.'" | sudo tee -a /etc/hosts');
+            file_put_contents($findDomain->domain_public . '/index.php', '<?php echo "site-is-ok, "; echo phpversion(); ?>');
+            $domainHomePageContent = file_get_contents('http://' . $hostingSubscription->domain);
 
+            $this->assertTrue(Str::contains($domainHomePageContent, 'site-is-ok, ' . $phpVersion['full']));
+
+            break;
+        }
     }
+
 //
 //    public function testHostingSubscriptionDeletion(): void
 //    {

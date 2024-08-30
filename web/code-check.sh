00afdc1a5a0530d00bd5982a3e552cd8656bdeb1
Bozhidar <bobicloudvision@gmail.com>
2024-08-30 15:41:50 +0300

    update

--- web/code-check.sh
+++ web/code-check.sh
@@ -1,2 +1,2 @@
-php -d memory_limit=4G ./vendor/bin/phpstan analyse config database app
+php -d memory_limit=4G ./vendor/bin/phpstan analyse config database app --level=0
 #php -d memory_limit=4G ./vendor/bin/pint --test

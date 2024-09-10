e67464716127c3cb96cb21292f06832f6c3de922
Bozhidar <bobicloudvision@gmail.com>
2024-09-10 22:05:15 +0300

    Create omega-repo.blade.php

---
+++ web/resources/views/server/samples/almalinux/repos/omega-repo.blade.php
@@ -0,0 +1,7 @@
+# Name:  PanelOmega AlmaLinux $releasever - $basearch
+# URL: http://www.panelomega.com/
+[omega]
+name = PanelOmega AlmaLinux $releasever - $basearch
+mirrorlist = https://license.panelomega.com/mirrorlist/almalinux/$releasever/$basearch
+enabled = 1
+protect = 0
